c4f6e4a8955e0b583d55c44b0aa42261f26d1415
SjjAugust <278238975@qq.com>
2021-02-24 17:45:52 +0800

    change para

--- include/SimplePipeline.h
+++ include/SimplePipeline.h
@@ -360,7 +360,7 @@ protected:
                     )
             {
                 DrawTrack(frame, *track);
-                std::string label = track->m_lastRegion.m_type + ": " + std::to_string((int)(track->m_lastRegion.m_confidence * 100)) + " %" + "track_id:" + track->m_trackID;
+                std::string label = track->m_lastRegion.m_type + ": " + std::to_string((int)(track->m_lastRegion.m_confidence * 100)) + " %" + "track_id:" + std::to_string(track->m_trackID);
                 //std::string label = std::to_string(track->m_trace.m_firstPass) + " | " + std::to_string(track->m_trace.m_secondPass);
                 int baseLine = 0;
                 cv::Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
